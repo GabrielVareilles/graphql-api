cdd3f4ba776e1e73c0e30c5ff9c53c3b9171e3a4
gabriel vareilles <gabriel.vareilles@gmail.com>
2018-04-23 17:26:00 +0200

    grant access to graphiql in production environment

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-
-  if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
-  end
+  mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
 
   post '/graphql', to: 'graphql#execute'
 end
